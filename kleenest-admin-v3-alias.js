ff215f03fe528aa161847fc65b62246b621add77
matthagersenior <matthagersr@gmail.com>
2026-08-16 17:48:15 -0500

    Wire audited Admin core as canonical

---
+++ kleenest-admin-v3-alias.js
@@ -0,0 +1,2 @@
+/* Make the audited Admin core the canonical shell target. */
+(function(g){'use strict';if(g.KleenestPlatformAdminCoreV3)g.KleenestPlatformAdminCoreV2=g.KleenestPlatformAdminCoreV3;})(window);
